4053073a152ecf4679b638ee8e1c879d5dbd9e52
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-03-10 14:43:05 -0500

    Time: 3 ms (86.48%), Space: 43.6 MB (64.73%)

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,5 +1,6 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
+        
         Arrays.sort(nums);
         int[] memo = new int[nums.length];
         Arrays.fill(memo,-1);
@@ -19,14 +20,17 @@ class Solution {
         
         int st = i + 1;
         
+        // Update number of points with curVal 
         while(st<nums.length && nums[st]==curVal)
             curSum += nums[st++];
         
         int prevSt = st;
         
+        // Skip all the values of (curVal+1)
         while(st<nums.length && nums[st]==curVal+1)
             st++;
         
+        // Ans = Max(take curVal and Skip next, do not take curVal) 
         return memo[i] = Math.max(curSum+recurse(nums, st, memo), recurse(nums, prevSt, memo));
     }
 }
