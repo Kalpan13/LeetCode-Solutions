6ea50da2dbe609cd34fefd5ddb800c32d821a13f
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-06-26 17:56:01 -0400

    Time: 3 ms (87.14%), Space: 55.5 MB (24.47%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,13 +1,12 @@
 class Solution {
     List<List<Integer>> paths;
     boolean[] visited;
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph)      {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph){
+        
         int n = graph.length;
         visited = new boolean[n];
         paths = new ArrayList<>();
-        
         List<Integer> curPath = new ArrayList<>();
-        // curPath.add(0);
         
         findPaths(0, curPath, graph, n);
         return paths;
@@ -16,14 +15,11 @@ class Solution {
     public void findPaths(int root, List<Integer> curPath, int[][] graph, int n)
     {
         
-        // System.out.println("Current Node : "+root + " Curpath : "+curPath);
-        
         if(visited[root])
             return;
         
         if(root == n-1)  // Desitnation Reached
         {
-            // System.out.println("Target Reached. Adding Path to answer");
             curPath.add(root);
             paths.add(new ArrayList(curPath));
             curPath.remove(curPath.size()-1);
@@ -35,12 +31,10 @@ class Solution {
         curPath.add(root);    // Add to path
         for(int neigh : graph[root]) // Explore neighbors
         {
-            // System.out.println("Exploring neighbor : "+neigh);
             findPaths(neigh, curPath, graph, n);
         }
-        curPath.remove(curPath.size()-1);
-        visited[root] = false;
-        // System.out.println("Completed Visiting : "+root + " CurPath : "+curPath);
+        curPath.remove(curPath.size()-1);  // Remove Current Node from path
+        visited[root] = false; // Mark as unvisited
         return;
             
     }
