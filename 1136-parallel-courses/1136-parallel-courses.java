1b568fc508cf855832ed60cbfb6d7af1b4ea8145
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-06-28 22:14:03 -0400

    Time: 36 ms (13.74%), Space: 58.1 MB (7.92%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -0,0 +1,54 @@
+class Solution {
+    Map<Integer,Set<Integer>> prereqMap;
+    public int minimumSemesters(int n, int[][] relations) {
+        prereqMap = new HashMap<>();
+        int coursesRemaining = n;
+        int inDegree[] = new int[n+1];
+        
+        // Create a dependency graph for courses and their prereqs
+        for(int[] relation : relations)
+        {
+            int course = relation[1];
+            int prereq = relation[0];
+            prereqMap.putIfAbsent(course, new HashSet<>());
+            prereqMap.putIfAbsent(prereq, new HashSet<>());
+            prereqMap.get(prereq).add(course);
+            
+            if(prereqMap.get(course).contains(prereq)) // Cycle
+                return -1; 
+            
+            inDegree[course]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1;i<n;i++)     // Take courses with no prereq
+        {
+            if(inDegree[i]==0)
+                q.offer(i);
+        }
+        
+        if(q.isEmpty())    // No course with 0 prereq
+            return -1;
+        int semestersTaken = 0;
+        
+        while(!q.isEmpty())
+        {
+            int qSize = q.size();
+            semestersTaken++;
+            for(int i=0;i<qSize;i++)
+            {
+                int currCourse = q.poll();
+                coursesRemaining--;
+                
+                for(Integer course: prereqMap.getOrDefault(currCourse, new HashSet<>()))
+                {
+                    inDegree[course]--;
+                    if(inDegree[course]==0)
+                        q.offer(course);
+                }
+            }
+        }
+        return coursesRemaining==0?semestersTaken:-1;
+        
+    }
+}
