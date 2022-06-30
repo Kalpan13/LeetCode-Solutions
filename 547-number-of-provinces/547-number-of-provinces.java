2f4737c3ca23da71ea4bd4334381b7c63e624d4f
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-06-30 00:49:37 -0400

    Time: 3 ms (46.30%), Space: 54.9 MB (7.00%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -0,0 +1,66 @@
+class Solution {
+    
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        UnionFind uf = new UnionFind(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1 && i!=j)
+                {
+                    uf.union(i,j);
+                }
+                    
+            }
+        }
+        int provinces = 0;
+    
+        for(int i=0;i<n;i++)
+        {
+            if(uf.find(i)==i)
+                provinces++;
+        }
+        return provinces;
+    }
+    class UnionFind{
+        int rank[];
+        int parent[];
+        
+        UnionFind(int n)
+        {
+            rank = new int[n];
+            Arrays.fill(rank,1);
+            parent = new int[n];
+            for(int i=0;i<n;i++)
+                parent[i] = i;
+        }
+        public int find(int x)
+        {
+            if(parent[x]==x)
+                return x;
+            else return parent[x] = find(parent[x]);
+        }
+        public void union(int a, int b)
+        {
+            int parentA = find(a);
+            int parentB = find(b);
+            
+            if(parentA==parentB)
+                return;
+            if(rank[parentA]>rank[parentB])
+            {
+                parent[parentB] = parentA;
+            }
+            else if(rank[parentA]<rank[parentB])
+            {
+                parent[parentA] = parentB;
+            }
+            else
+            {
+                rank[parentA]+= rank[parentB];
+                parent[parentB] = parentA;
+            }
+        }
+    }
+}
