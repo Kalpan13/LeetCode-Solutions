7db3f03d6e847f0160a58029403414bb04ebb3f8
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-06-30 21:16:22 -0400

    Time: 24 ms (51.00%), Space: 54.2 MB (69.75%) - LeetHub

---
+++ 1245-tree-diameter/1245-tree-diameter.java
@@ -0,0 +1,59 @@
+class Solution {
+    List<List<Integer>> graph;
+    int farthestFromStart = -1;
+    int maxDistanceFromStart = 0;
+    boolean[] visited;
+    int n;
+    public int treeDiameter(int[][] edges) {
+        // Start from random point. Find the farthest node (f1)
+        // Start from f1. Find the farthest node (f2)
+        
+        // Answer = distance(f1,f2)
+        if(edges.length==0)
+            return 0;
+        if(edges.length==1)
+            return 1;
+        n = edges.length+1;
+        graph = new ArrayList<>();
+        visited = new boolean[n];
+        
+        buildGraph(edges);
+        findFarthest(0, 0);
+        Arrays.fill(visited,false);
+        
+        findFarthest(farthestFromStart,0);
+        return maxDistanceFromStart;
+    }
+    public void buildGraph(int[][] edges)
+    {
+        for(int edge[] : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            while(graph.size()<=Math.max(u,v))
+                graph.add(new ArrayList<>());
+            
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+    }
+    public void findFarthest(int startNode, int distance)
+    {
+        visited[startNode] = true;
+        if(distance>maxDistanceFromStart)
+        {
+            farthestFromStart = startNode;
+            maxDistanceFromStart = distance;
+        }
+            
+        
+        for(int neighbor:graph.get(startNode))
+        {
+            if(!visited[neighbor])
+            {
+                findFarthest(neighbor, distance+1);
+            }
+        }
+        
+    }
+}
