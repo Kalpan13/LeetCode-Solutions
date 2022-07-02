58ec08eac368b165024042131d789434b070787b
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-07-01 20:45:47 -0400

    Time: 161 ms (42.39%), Space: 139.4 MB (9.54%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,53 @@
+class Solution {
+    Map<Integer,List<Integer>> graph;
+    Map<Integer,List<Integer>> revGraph;
+    int citiesConnected = 1; 
+    int edgeCount = 0;
+    boolean[] visited;
+    public int minReorder(int n, int[][] connections) {
+        graph = new HashMap<>();
+        revGraph = new HashMap<>();
+        visited = new boolean[n];
+        for(int connection[] : connections)
+        {
+            int from = connection[0];
+            int to = connection[1];
+            
+            graph.putIfAbsent(from, new ArrayList<>());
+            revGraph.putIfAbsent(to, new ArrayList<>());
+            graph.get(from).add(to);
+            revGraph.get(to).add(from);
+        }
+        reverseEdge(0,n);
+        return edgeCount;
+            
+    }
+    public void reverseEdge(int node, int n)
+    {
+        if(visited[node])
+            return;
+        
+        visited[node] = true;
+        List<Integer> next = new ArrayList<>();
+        
+        // Reverse edges from node to X
+        for(int neigh: graph.getOrDefault(node, new ArrayList<>()))
+        {
+            if(visited[neigh])
+                continue;
+            next.add(neigh);
+            // System.out.println("Counting edge from : "+node +" to : "+ neigh);
+            edgeCount++;
+        }
+        
+        // Directly add edges from all Xs to node
+        for(int neigh: revGraph.getOrDefault(node, new ArrayList<>()))
+        {
+            if(visited[neigh])
+                continue;
+            next.add(neigh);
+        }
+        for(int next_neigh : next)
+            reverseEdge(next_neigh, n);
+    }
+}
