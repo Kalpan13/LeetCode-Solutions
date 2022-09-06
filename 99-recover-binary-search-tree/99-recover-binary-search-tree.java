aeca15486ac19650d78a96fb1401f162e39aa607
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-09-06 11:59:48 -0400

    Time: 4 ms (46.02%), Space: 47.3 MB (49.71%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public void recoverTree(TreeNode root) {
+        List<Integer> inorder = new ArrayList<>();
+        buildInorder(root, inorder);
+        int[] swapped = findSwappedElements(inorder);
+        recoverSwapped(root, 0, swapped[0], swapped[1]);
+    }
+    public void buildInorder(TreeNode root, List<Integer> inorder)
+    {
+        
+        if(root == null)
+            return;
+        
+        buildInorder(root.left, inorder);
+        inorder.add(root.val);
+        buildInorder(root.right, inorder);
+    }
+    public int[] findSwappedElements(List<Integer> inorder)
+    {
+        int n = inorder.size();
+        boolean firstSwappedFound = false;
+        int x = -1;
+        int y = -1;
+        for(int i=0;i<n-1;i++)
+        {
+            if(inorder.get(i+1)<inorder.get(i))
+            {
+                y = inorder.get(i+1);
+                if(firstSwappedFound)
+                {
+                    break;
+                }
+                else
+                {
+                    x = inorder.get(i);      
+                    firstSwappedFound = true;
+                }
+            }
+        }
+        return new int[]{x,y};
+    }
+    public void recoverSwapped(TreeNode root, int recoveredCount, int x, int y)
+    {
+        if(root==null)
+            return;
+        if(root.val==x || root.val==y)
+        {
+            root.val = root.val==x?y:x;
+            recoveredCount++;
+            if(recoveredCount==2)
+                return;
+        }
+        recoverSwapped(root.left, recoveredCount, x, y);
+        recoverSwapped(root.right, recoveredCount, x, y);
+        
+    }
+}
