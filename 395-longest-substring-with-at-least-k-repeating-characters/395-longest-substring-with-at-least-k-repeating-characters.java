bcd42baffaa2ec489ec1f1373fc593d7856099dc
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-09-14 01:23:01 -0400

    Time: 13 ms (37.54%), Space: 41.8 MB (80.76%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,26 +1,54 @@
 class Solution {
-    
     public int longestSubstring(String s, int k) {
-        int frq[] = new int[26];
-        int n = s.length();
+        int uniqChars = findUniqueChars(s);
+        int[] frq = new int[26];
         int maxLen = 0;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=uniqChars;i++)
         {
+            int start = 0;
             Arrays.fill(frq,0);
-            for(int j=i;j<n;j++)
+            int curUniqueChars = 0;
+            for(int end=0;end<s.length();end++)
             {
-                frq[s.charAt(j)-'a']++;
-                if(isValid(frq,k))
-                    maxLen = Math.max(maxLen, j-i+1);
+                char c = s.charAt(end);
+                if(frq[c-'a']==0)
+                    curUniqueChars++;
+                frq[c-'a']++;
+                
+                while(curUniqueChars>i)
+                {
+                    char startC = s.charAt(start);
+                    frq[startC-'a']--;
+                    if(frq[startC-'a']==0)
+                        curUniqueChars--;
+                    start++;
+                }
+                // System.out.println("Start : "+start+ "End : "+end + "isValid :"+);
+                if(isValid(frq, k))
+                    maxLen = Math.max(maxLen, end-start+1);
             }
+            
         }
         return maxLen;
+    }
+    public int findUniqueChars(String s)
+    {
+        Set<Character> hs = new HashSet<>();
+        int count = 0;
+        for(char c:s.toCharArray())
+        {
+            if(hs.contains(c))
+                continue;
+            hs.add(c);
+            count++;
+        }
+        return count;
     }
     public boolean isValid(int[] frq, int k)
     {
         for(int i=0;i<26;i++)
         {
-            if(frq[i]<k && frq[i]>0)
+            if(frq[i]>0 && frq[i]<k)
                 return false;
         }
         return true;
