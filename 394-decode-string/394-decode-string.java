232b6d1e593437c4698df0a880756e4b51a54d69
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-09-29 19:55:41 -0400

    Time: 2 ms (62.46%), Space: 42 MB (64.24%) - LeetHub

---
+++ 394-decode-string/394-decode-string.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> st = new Stack<>();
+        Stack<Integer> countSt = new Stack<>();
+        int i=0;
+        while(i<s.length())
+        {
+            char c = s.charAt(i);
+            //System.out.println("Current char : "+c);
+            if(Character.isDigit(c))
+            {
+                int num = c-'0';
+                
+                int j = i+1;
+                while(j<s.length() && Character.isDigit(s.charAt(j)))
+                {
+                    num *= 10;
+                    num += (s.charAt(j)-'0');
+                    j++;
+                }
+                i = j;
+                countSt.push(num);
+                //System.out.println("Added num : "+num + " to countSt : "+ countSt);
+                continue;
+            }
+            else if(c=='[')
+            {
+                st.push(new String("["));
+            }
+            else if(c==']')
+            {
+                StringBuffer sb = new StringBuffer();
+                while(!st.peek().equals("["))
+                    sb.insert(0, st.pop());
+                
+                st.pop();
+                
+                int num = countSt.pop();
+                //System.out.println("Frequency of word :"+sb+ " is "+num);
+                StringBuffer ans = new StringBuffer();
+                while(num-->0)
+                    ans.append(sb);
+                
+                st.push(ans.toString());
+            }
+            else
+            {
+                st.push(Character.toString(c));
+            }
+            i++;
+        }
+        StringBuffer decodedString = new StringBuffer();
+        while(!st.isEmpty())
+            decodedString.insert(0,st.pop());
+        return decodedString.toString();
+    }
+    
+}
