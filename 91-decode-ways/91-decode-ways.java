2edccc324c06db432e220e44cf900b05a4159ac8
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-10-02 16:51:16 -0400

    Time: 2 ms (67.68%), Space: 42.4 MB (42.50%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,42 @@
+class Solution {
+    int memo[];
+    int count = 0;
+    Set<StringBuffer> seen;
+    public int numDecodings(String s) {
+        count = 0;
+        seen = new HashSet<>();
+        int n = s.length();
+        memo = new int[n];
+        Arrays.fill(memo, -1);
+        int ways = countWays(s, 0, new StringBuffer());
+        return memo[0]==-1?0:memo[0];
+    
+    }
+    public int countWays(String s, int ind, StringBuffer cur)
+    {
+        if(ind>=s.length())
+        {
+            count++;
+            return 1;
+        }
+        if(memo[ind]!=-1)
+            return memo[ind];
+        if(s.charAt(ind)>='1' && s.charAt(ind)<='9')
+        {
+            if(ind<s.length()-1 && ((s.charAt(ind)=='2' && s.charAt(ind+1)>='0' && s.charAt(ind+1)<='6') || s.charAt(ind)=='1'))
+            {
+                
+                int c1 = countWays(s, ind+1, cur);
+                int c2 = countWays(s, ind+2, cur);
+                
+                return memo[ind] = (c1+c2);    
+            }
+            else
+            {
+                int c1 = countWays(s, ind+1, cur);
+                return memo[ind] = (c1);
+            }  
+        }   
+        return 0;
+    }
+}
