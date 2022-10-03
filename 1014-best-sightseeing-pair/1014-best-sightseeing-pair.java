0088eb468925fdb9f87f33af03cac7b9db55daca
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-10-03 19:35:58 -0400

    Time: 4 ms (75.72%), Space: 63.5 MB (73.06%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int maxBestPairSum = 0;
+        int maxSightSee1 = values[0];
+        int maxSightSee1Effective = values[0];
+        
+        for(int i=1;i<values.length;i++)
+        {
+            maxSightSee1Effective--;
+            maxBestPairSum = Math.max(maxBestPairSum, maxSightSee1Effective+values[i]);
+            if(values[i]>maxSightSee1Effective)
+            {
+                maxSightSee1 = values[i];
+                maxSightSee1Effective = values[i];
+            }
+                
+        }
+        return maxBestPairSum;
+    }
+}
