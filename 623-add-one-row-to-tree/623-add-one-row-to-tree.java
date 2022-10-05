8f2c82921dbc36357545ef5ce146c4b6f597fc68
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-10-05 15:07:51 -0400

    Time: 1 ms (88.59%), Space: 45 MB (71.90%) - LeetHub

---
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        
+        if(depth==1)
+        {
+            TreeNode newRoot = new TreeNode(val);
+            newRoot.left = root;
+            return newRoot;
+        }
+        // return root;
+        helper(root, val, depth, 1);
+        return root;
+    }
+    public void helper(TreeNode root, int val, int depth, int curDepth)
+    {
+        curDepth++;
+        
+        if(root==null)
+            return;
+        // System.out.println("Depth : "+depth + " curDepth : "+curDepth + " root :"+root.val);
+        if(curDepth==depth)
+        {
+            // System.out.println("Changing Nodes");
+            TreeNode newNodeLeft = new TreeNode(val);
+            if(root.left!=null)
+                newNodeLeft.left = root.left;
+            
+            TreeNode newNodeRight = new TreeNode(val);
+            if(root.right!=null)
+            newNodeRight.right = root.right;
+            
+            root.left = newNodeLeft;
+            root.right = newNodeRight;
+            return;
+        }
+        helper(root.left, val, depth, curDepth);
+        helper(root.right, val, depth, curDepth);
+        
+    }
+}
