24aae7cd32c5bb5bd1bc3e9b3fa696026c7b3b9b
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-10-22 21:53:36 -0400

    Time: 5 ms (55.54%), Space: 40.4 MB (94.67%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -13,22 +13,17 @@ class Solution {
         List<String> ans = new ArrayList<>();
         for(String sentence : sentences)
         {
-            //sentence = sentence
-            
             ans.add(sentence.substring(0, sentence.length()-1));
         }
         return ans;
     }
     public List<String> traverse(int i, String s)
     {
-        // System.out.println("i : "+i);
-        //System.out.println("New i : "+i);
         if(i==s.length()) // End of String
         {
             
             List<String> temp = new ArrayList<>();
             temp.add("");
-            // System.out.println("temp " +temp);
             return temp;
         }
             
@@ -39,32 +34,19 @@ class Solution {
         for(int j=i;j<s.length();j++)
         {
             String curStr = s.substring(i, j+1);
-            // System.out.println("curStr : "+curStr);
             if(wordDictSet.contains(curStr))
             {
-                //System.out.println("curStr found in str : "+curStr);
-                
+                // if current substring exist in words set check for the remaining substring
                 memo.putIfAbsent(i, new ArrayList<>());
                 List<String> sentences = traverse(j+1, s);
-                //System.out.println("Sentences :"+sentences);
                 if(sentences==null)
                     continue;
-                //String 
                 for(String sentence: sentences)
                 {
-                    // if(sentence.length()==0)
-                    // {
-                    //     sentence+= curStr;
-                    // }
-                    // else
-                    // {
-                    //     sentence+=(" "+(curStr));
-                    // }
                     memo.get(i).add(curStr+" "+sentence);
                 }
             }
         }
         return memo.get(i);
-        
     }
 }
