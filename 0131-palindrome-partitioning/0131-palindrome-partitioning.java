9538c99851c26404690c67db441568dd3a993e6c
Kalpan Tumdi <31406929+Kalpan13@users.noreply.github.com>
2022-10-31 16:37:32 -0400

    Time: 21 ms (67.22%), Space: 135.8 MB (72.30%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,6 +1,9 @@
 class Solution {
+    Boolean memo[][];
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
+        memo = new Boolean[s.length()][s.length()];
+        
         backtrack(0, ans, new ArrayList<>(), s);
         return ans;
     }
@@ -14,11 +17,17 @@ class Solution {
         
         for(int i=pos;i<s.length();i++)
         {
-            if(isPalindrome(s, pos, i))
+            
+            if((memo[pos][i]!=null && memo[pos][i]==true) || isPalindrome(s, pos, i))
             {
+                memo[pos][i] = true;
                 curList.add(s.substring(pos, i+1));
                 backtrack(i+1, ans, curList, s);
                 curList.remove(curList.size()-1);
+            }
+            else
+            {
+                memo[pos][i] = false;
             }
         }
     }
